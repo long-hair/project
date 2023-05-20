4430c0261f5198473f130eb0ed27af7948f81bec
wmj <394919946@qq.com>
2023-05-20 23:31:07 +0800

    project: 生成项目

---
+++ src/business/constants/router.ts
@@ -0,0 +1,6 @@
+export enum Router {
+  Home = '/home',//首页
+  About = '/about',//关于
+  Contact = '/contact',//联系我们
+  Recruitment = '/recruitment',//招聘
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * 首页
+ * 
+ * @returns 
+ */
+const Home:React.FC = () => {
+  return (
+    <div>
+      1111
+    </div>
+  );
+};
+
+export default Home;
